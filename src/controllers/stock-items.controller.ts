a8225eec8b5a3c96820fd4072e23007cdd9b884d
Joseph Tyler DiBartolo <joseph.dibartolo@ibm.com>
2020-01-03 12:23:10 -0500

    Adds stock items controller

---
+++ src/controllers/stock-items.controller.ts
@@ -0,0 +1,11 @@
+import {GET, Path} from 'typescript-rest';
+
+@Path('stock-items')
+export class StockItemsController {
+
+    @GET
+    async listStockItems(): Promise<any[]> {
+        return [];
+    }
+
+}
